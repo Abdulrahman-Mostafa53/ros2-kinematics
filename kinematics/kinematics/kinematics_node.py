97a2e98cac4ddf2a08589bedbee0ba38845adbb7
wael26214-blip <wael26214@gmail.com>
2026-09-04 06:16:41 +0300

    Update kinematics_node.py

--- kinematics/kinematics/kinematics_node.py
+++ kinematics/kinematics/kinematics_node.py
@@ -1,6 +1,14 @@
 import rclpy
 from rclpy.node import Node
 import kinematics.robot_kinematics
+from kinematics.robot_kinematics import (
+    DiffDriveKinematics, 
+    MecanumKinematics, 
+    ThreeWheelOmniKinematics, 
+    FourWheelOmniKinematics
+)
+from geometry_msgs.msg import Twist
+from std_msgs.msg import Float64MultiArray
 
 
 class KinematicsNode(Node):
@@ -20,6 +28,64 @@ class KinematicsNode(Node):
             self.get_parameters(['drive_type','track_width','wheelbase','wheel_radius'])
         )
         self.get_logger().info(self.drive_type.value)
+        self.set_Kinematics()
+
+
+        self.subscription = self.create_subscription(
+            Twist,
+            '/cmd_vel',
+            self.cmd_vel_callback,
+            10
+        )
+
+
+        self.publisher = self.create_publisher(
+            Float64MultiArray,
+            '/movement_speed',
+            10
+        )    
+
+
+
+    def cmd_vel_callback(self, msg: Twist):
+     if self.kinematics is None:
+        self.get_logger().error("Kinematics engine is not initialized!")
+        return
+     
+     vx = msg.linear.x
+     vy = msg.linear.y
+     wz = msg.angular.z
+
+        
+     wheel_speeds = self.kinematics.inverse(vx, vy, wz)
+
+        
+     wheels_array = Float64MultiArray()
+     wheels_array.data = wheel_speeds.tolist()
+     self.publisher.publish(wheels_array)
+
+
+
+
+    def set_Kinematics(self):
+     d_type = self.drive_type.value
+     L = self.track_width.value
+     W = self.wheel_base.value
+     R = self.wheel_radius.value
+
+     if d_type in ["diff", "diff_drive"]:
+        self.kinematics = DiffDriveKinematics(L, R, W)
+     elif d_type == "mecanum":
+        self.kinematics = MecanumKinematics(L, R, W)
+     elif d_type == "three_wheel_omni":
+        self.kinematics = ThreeWheelOmniKinematics(L, R, W)
+     elif d_type == "four_wheel_omni":
+        self.kinematics = FourWheelOmniKinematics(L, R, W)
+     else:
+        self.get_logger().error(f"Unsupported drive type: {d_type}")
+        self.kinematics = None  
+      
+
 
 
 def main():
